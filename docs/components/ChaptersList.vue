04889fda29e4f81f3a8239ea730dde23d0a2bffc
Dmitry Pupinin <dlnsk@mail.ru>
2023-11-20 15:54:49 +0700

    Docs for outline

---
+++ docs/components/ChaptersList.vue
@@ -0,0 +1,27 @@
+<script>
+export default {
+  name: 'ChaptersList',
+  props: {
+    items: Array,
+  },
+  methods: {
+    onChapterClick: function (e) {
+      this.$emit('chapterClick', e)
+    },
+  },
+}
+</script>
+
+<template>
+  <ol>
+    <li v-for="item in items">
+      <a href="#" @click.prevent="$emit('chapterClick', item.destination)">{{ item.title }}</a>
+      <div v-if="item.items.length">
+        <ChaptersList
+          :items="item.items"
+          @chapterClick="onChapterClick"
+        ></ChaptersList>
+      </div>
+    </li>
+  </ol>
+</template>
---
+++ docs/components/TOC.vue
@@ -0,0 +1,73 @@
+<script setup>
+import { ref, triggerRef, watchEffect } from 'vue';
+import { VuePDF, getPDFDestination, usePDF } from '@tato30/vue-pdf';
+import { withBase } from '@vuepress/client';
+import ChaptersList from './ChaptersList.vue';
+
+const { pdf, info } = usePDF(withBase('/example_045.pdf'))
+const eventValue = ref({})
+const outlineTree = ref([])
+
+watchEffect(() => {
+  if (info.value.outline !== undefined) {
+    outlineTree.value = info.value.outline.map(function convert(node) {
+      return {
+        title: node.title,
+        destination: getPDFDestination(info.value.document, node.dest),
+        items: node.items.map((item) => {
+          return convert(item)
+        }),
+      }
+    })
+  }
+})
+triggerRef(info)
+
+function onChapterClick(value) {
+  value.then(v => {
+    console.log(v)
+    eventValue.value = v
+  })
+}
+</script>
+
+<template>
+  <div id="toc_wrapper">
+    <div class="toc">
+      <ChaptersList
+        :items="outlineTree"
+        @chapterClick="onChapterClick"
+      >
+      </ChaptersList>
+    </div>
+    <div>
+      <div class="language-json" data-ext="json">
+        <pre class="language-json"><code>{{ eventValue }}</code></pre>
+      </div>
+
+      <div class="container">
+        <VuePDF :pdf="pdf" :scale="0.75" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<style>
+#toc_wrapper {
+  display: flex;
+  flex-direction: row;
+}
+#toc_wrapper .toc {
+  width: 300px;
+  background-color: #eaeaea;
+}
+#toc_wrapper ol ol {
+  padding-left: 20px;
+}
+#toc_wrapper ol {
+  padding-left: 2em;
+}
+#toc_wrapper a {
+  color: black;
+}
+</style>
