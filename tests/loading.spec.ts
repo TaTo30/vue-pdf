7434f2e2b879503a09d23add54d2ca2076d9a381
TaTo30 <aldo.hernandez3012@gmail.com>
2024-01-02 14:39:48 -0600

    more tests

--- tests/loading.spec.ts
+++ tests/loading.spec.ts
@@ -12,7 +12,7 @@ beforeAll(async () => {
   await vi.waitUntil(() => pdf.value, { timeout: 5000 })
 })
 
-test('Load/Mount component', () => {
+test('Load/Mount component', async () => {
   expect(pdf.value).toBeTruthy()
   expect(pages.value).toBe(14)
 
@@ -23,6 +23,24 @@ test('Load/Mount component', () => {
   })
 
   expect(wrapper).toBeTruthy()
+
+  await vi.waitUntil(() => wrapper.emitted('loaded'), {
+    timeout: 5000,
+  })
+
+  expect(wrapper.emitted('loaded')).toHaveLength(1)
+  expect(wrapper.emitted('loaded')![0]).toEqual([
+    {
+      viewBox: [0, 0, 612, 792],
+      scale: 1,
+      rotation: 0,
+      offsetX: 0,
+      offsetY: 0,
+      transform: [1, 0, 0, -1, 0, 792],
+      width: 612,
+      height: 792,
+    },
+  ])
 
   // console.log(wrapper.get('canvas').element.toDataURL('image/png'))
 })
